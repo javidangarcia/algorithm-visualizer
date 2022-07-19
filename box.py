bdcc3e6c41fc6807372ccc5c19c0cf10f225dd54
Javier Garcia <javierdangarcia@gmail.com>
2022-07-19 19:48:18 -0400

    added blocks and canvas

--- box.py
+++ box.py
@@ -5,6 +5,12 @@ class Box:
     def __init__(self, col, row):
         self.x = col
         self.y = row
+        self.start = False
+        self.wall = False
+        self.target = False
 
     def draw(self, window, color):
         pygame.draw.rect(window, color, (self.x * BOX_WIDTH, self.y * BOX_HEIGHT, BOX_WIDTH - 2, BOX_HEIGHT - 2))
+
+
+        
--- main.py
+++ main.py
@@ -4,32 +4,62 @@ from box import Box
 
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 
-def create_grid():
-    grid = []
+grid = []
+for col in range(COLS):
+    boxes = []
     for row in range(ROWS):
-        boxes = []
-        for col in range(COLS):
-            boxes.append(Box(col, row))
-        grid.append(boxes)
-    return grid
+        boxes.append(Box(col, row))
+    grid.append(boxes)
+
+start_box = grid[0][0]
+start_box.start = True
 
 def main():
-    grid = create_grid()
+    begin_search = False
+    target_box_set = False
 
     run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
+                run = False
+
+            elif event.type == pygame.MOUSEMOTION:
+                x, y = pygame.mouse.get_pos()
+
+                if event.buttons[0]:
+                    col = x // BOX_WIDTH
+                    row = y // BOX_HEIGHT
+                    grid[col][row].wall = True
+            
+            elif event.type == pygame.MOUSEBUTTONDOWN and target_box_set == False:
+                    if event.button == 3:
+                        col = x // BOX_WIDTH
+                        row = y // BOX_HEIGHT
+                        target_box = grid[col][row]
+                        target_box.target = True
+                        target_box_set = True
+            
+            elif event.type == pygame.KEYDOWN and target_box_set:
+                begin_search = True
+
 
         WINDOW.fill(BLACK)
 
-        for row in range(ROWS):
-            for col in range(COLS):
+        for col in range(COLS):
+            for row in range(ROWS):
                 box = grid[col][row]
                 box.draw(WINDOW, GRAY)
+                if box.start:
+                    box.draw(WINDOW, BLUE)
+                if box.wall:
+                    box.draw(WINDOW, WHITE)
+                if box.target:
+                    box.draw(WINDOW, RED)
 
         pygame.display.update()
+
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
