a48124085d6406f7f4b880b7c3ccad0fc5f9cec3
Javier Garcia <javierdangarcia@gmail.com>
2022-07-26 21:09:42 -0400

    added pathfinding algorithm

---
+++ blocks.py
@@ -0,0 +1,27 @@
+from constants import *
+import pygame
+
+class Block:
+    def __init__(self, col, row):
+        self.x = col
+        self.y = row
+        self.start = False
+        self.wall = False
+        self.target = False
+        self.queued = False
+        self.visited = False
+        self.neighbors = []
+        self.prior = None
+
+    def set_neighbors(self, canvas):
+        if self.x > 0:
+            self.neighbors.append(canvas[self.x - 1][self.y])
+        if self.x < COLS - 1:
+            self.neighbors.append(canvas[self.x + 1][self.y])
+        if self.y > 0:
+            self.neighbors.append(canvas[self.x][self.y - 1])
+        if self.y < ROWS - 1:
+            self.neighbors.append(canvas[self.x][self.y + 1])
+
+    def draw(self, window, color):
+        pygame.draw.rect(window, color, (self.x * BLOCK_WIDTH, self.y * BLOCK_HEIGHT, BLOCK_WIDTH - 2, BLOCK_HEIGHT - 2))
--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 import pygame
 from constants import *
-from box import Box
+from blocks import Block
 
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Dijkstra Algorithm Visualizer")
 
-grid = []
+canvas = []
+queue = []
+path = []
+
 for col in range(COLS):
-    boxes = []
+    blocks = []
     for row in range(ROWS):
-        boxes.append(Box(col, row))
-    grid.append(boxes)
+        blocks.append(Block(col, row))
+    canvas.append(blocks)
 
-start_box = grid[0][0]
-start_box.start = True
+for col in range(COLS):
+    for row in range(ROWS):
+        canvas[col][row].set_neighbors(canvas)
+
+start_block = canvas[0][0]
+start_block.start = True
+start_block.visited = True
+queue.append(start_block)
 
 def main():
     begin_search = False
-    target_box_set = False
+    target_block_set = False
+    searching = True
+    target_block = None
 
     run = True
     while run:
@@ -26,36 +38,58 @@ def main():
 
             elif event.type == pygame.MOUSEMOTION:
                 x, y = pygame.mouse.get_pos()
-
                 if event.buttons[0]:
-                    col = x // BOX_WIDTH
-                    row = y // BOX_HEIGHT
-                    grid[col][row].wall = True
+                    col = x // BLOCK_WIDTH
+                    row = y // BLOCK_HEIGHT
+                    canvas[col][row].wall = True
             
-            elif event.type == pygame.MOUSEBUTTONDOWN and target_box_set == False:
+            elif event.type == pygame.MOUSEBUTTONDOWN and target_block_set == False:
                     if event.button == 3:
-                        col = x // BOX_WIDTH
-                        row = y // BOX_HEIGHT
-                        target_box = grid[col][row]
-                        target_box.target = True
-                        target_box_set = True
+                        col = x // BLOCK_WIDTH
+                        row = y // BLOCK_HEIGHT
+                        target_block = canvas[col][row]
+                        target_block.target = True
+                        target_block_set = True
             
-            elif event.type == pygame.KEYDOWN and target_box_set:
+            elif event.type == pygame.KEYDOWN and target_block_set:
                 begin_search = True
 
+        if begin_search:
+            if len(queue) > 0 and searching:
+                current_block = queue.pop(0)
+                current_block.visited = True
+                if current_block == target_block:
+                    searching = False
+                    while current_block.prior != start_block:
+                        path.append(current_block.prior)
+                        current_block = current_block.prior
+                else:
+                    for neighbor in current_block.neighbors:
+                        if neighbor.queued == False and neighbor.wall == False:
+                            neighbor.queued = True
+                            neighbor.prior = current_block
+                            queue.append(neighbor)
+            else:
+                if searching:
+                    run = False
 
         WINDOW.fill(BLACK)
-
         for col in range(COLS):
             for row in range(ROWS):
-                box = grid[col][row]
-                box.draw(WINDOW, GRAY)
-                if box.start:
-                    box.draw(WINDOW, BLUE)
-                if box.wall:
-                    box.draw(WINDOW, WHITE)
-                if box.target:
-                    box.draw(WINDOW, RED)
+                block = canvas[col][row]
+                block.draw(WINDOW, GRAY)
+                if block.queued:
+                    block.draw(WINDOW, RED)
+                if block.visited:
+                    block.draw(WINDOW, GREEN)
+                if block in path:
+                    block.draw(WINDOW, BLUE)
+                if block.start:
+                    block.draw(WINDOW, BLUE)
+                if block.wall:
+                    block.draw(WINDOW, WHITE)
+                if block.target:
+                    block.draw(WINDOW, BLUE)
 
         pygame.display.update()
 
